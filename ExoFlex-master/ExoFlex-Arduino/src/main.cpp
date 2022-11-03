be2694f4fb9cc46736c9372d05215baf75ebe101
OJackson <olivier.jackson@hotmail.ca>
2022-11-02 20:32:51 -0400

    stepper mini test

--- ExoFlex-master/ExoFlex-Arduino/src/main.cpp
+++ ExoFlex-master/ExoFlex-Arduino/src/main.cpp
@@ -10,14 +10,16 @@ using namespace std;
 #define BAUD 115200        // Frequence de transmission serielle
 #define UPDATE_PERIODE 100 // Periode (ms) d'envoie d'etat general
 
-Servo dorsiflex_motor; // PIN 10
-Servo eversion_motor;  // PIN 11
+Servo dorsiflex_motor;               // PIN 10
+Servo eversion_motor;                // PIN 11
+AccelStepper stepper_tight(1, 2, 5); // STEP PIN 2, STEP DIR 5
 
 /*------------------------------ Global Variable ---------------------------------*/
 volatile bool shouldSend_ = false; // Ready to send message to serial flag
 volatile bool shouldRead_ = false; // Ready to read message to serial flag
 
-int pos = 0; // Dorsiflexion motor position
+int motor_pos = 0;   // Dorsiflexion/eversion motor position
+int stepper_pos = 0; // Tightening stepper pos
 
 SoftTimer timerSendMsg_; // Send message timer
 
@@ -42,6 +44,11 @@ void setup()
     // Servo init
     dorsiflex_motor.attach(10); // dorsiflexion motor attached to PIN 10
     eversion_motor.attach(11);  // eversion motor attached to PIN 11
+
+    // Stepper init
+    stepper_tight.setMaxSpeed(2000);     // À MODIFIER
+    stepper_tight.setAcceleration(1000); // À MODIFIER
+    stepper_tight.disableOutputs();
 }
 
 /*------------------------------ Main loop ---------------------------------*/
@@ -57,18 +64,33 @@ void loop()
         sendMsg();
     }
 
-    for (pos = 0; pos <= 180; pos += 1)
+    // Motor tests
+
+    for (motor_pos = 0; motor_pos <= 180; motor_pos += 1)
     { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
-        dorsiflex_motor.write(pos); // tell servo to go to position in variable 'pos'
-        eversion_motor.write(pos);  // tell servo to go to position in variable 'pos'
-        delay(20);                  // waits 15 ms for the servo to reach the position
+        dorsiflex_motor.write(motor_pos); // tell servo to go to position in variable 'motor_pos'
+        eversion_motor.write(motor_pos);  // tell servo to go to position in variable 'motor_pos'
+        delay(20);                        // waits 15 ms for the servo to reach the position
     }
-    for (pos = 180; pos >= 0; pos -= 1)
-    {                               // goes from 180 degrees to 0 degrees
-        dorsiflex_motor.write(pos); // tell servo to go to position in variable 'pos'
-        eversion_motor.write(pos);  // tell servo to go to position in variable 'pos'
-        delay(20);                  // waits 15 ms for the servo to reach the position
+    for (motor_pos = 180; motor_pos >= 0; motor_pos -= 1)
+    {                                     // goes from 180 degrees to 0 degrees
+        dorsiflex_motor.write(motor_pos); // tell servo to go to position in variable 'motor_pos'
+        eversion_motor.write(motor_pos);  // tell servo to go to position in variable 'motor_pos'
+        delay(20);                        // waits 15 ms for the servo to reach the position
+    }
+
+    // Stepper tests
+
+    for (stepper_pos = 0; stepper_pos <= 180; stepper_pos += 1)
+    {
+        stepper_tight.move(stepper_pos); // tell servo to go to position in variable 'pos'
+        delay(20);                       // waits 15 ms for the servo to reach the position
+    }
+    if (stepper_pos == 180)
+    { // Stop stepper
+        stepper_tight.stop();
+        stepper_tight.disableOutputs();
     }
 
     timerSendMsg_.update();
