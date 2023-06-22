ffaf0757b198166aeec2982c8620544f46aedf55
BigJack325 <BigJack325@users.noreply.github.com>
2023-06-22 03:05:10 +0000

    Apply Prettier changes

--- master/test.js
+++ master/test.js
@@ -1,6 +1,5 @@
-function   add   (  a, b  )   {
-    return  a + b;
-  }
-  
-  console.log("The sum is: " + add(2,3));
-  
+function add(a, b) {
+  return a + b;
+}
+
+console.log("The sum is: " + add(2, 3));
