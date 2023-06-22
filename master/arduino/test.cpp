c9f4fd795711a2ddbbb2402b07ac5d0e326eccdf
BigJack325 <BigJack325@users.noreply.github.com>
2023-06-22 13:16:49 +0000

    Apply Formatting changes

--- master/arduino/test.cpp
+++ master/arduino/test.cpp
@@ -1,8 +1,10 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
-int main(){
-std::vector<int>numbers={1,2,3,4,5};
-for(int i=0;i<numbers.size();i++){
-std::cout<<"Number:"<<numbers[i]<<std::endl;}
-return 0;}
+int main() {
+  std::vector<int> numbers = {1, 2, 3, 4, 5};
+  for (int i = 0; i < numbers.size(); i++) {
+    std::cout << "Number:" << numbers[i] << std::endl;
+  }
+  return 0;
+}
--- master/arduino/test.js
+++ master/arduino/test.js
@@ -1,3 +1,3 @@
-function   addNumbers   (  a , b  )
-  {   return  a    + b;
+function addNumbers(a, b) {
+  return a + b;
 }
