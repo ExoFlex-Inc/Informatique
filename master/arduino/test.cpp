ff413d02eb913792195400142b6af309daa98f52
Olivier <olivier.jackson@hotmail.ca>
2023-06-22 09:38:21 -0400

    test

--- master/arduino/test.cpp
+++ master/arduino/test.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-  std::vector<int> numbers = {1, 2, 3, 4, 5};
-  for (int i = 0; i < numbers.size(); i++) {
-    std::cout << "Number:" << numbers[i] << std::endl;
-  }
-  return 0;
+int main() 
+{
+std::vector<int> numbers = {1, 2, 3, 4, 5};
+for (int i = 0; i < numbers.size(); i++) {
+std::cout << "Number:" << numbers[i] << std::endl;
+}
+return 0;
 }
