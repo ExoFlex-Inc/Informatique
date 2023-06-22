feef72f54fb5769c9b1ebea5d00814c0b9dd3662
Olivier <olivier.jackson@hotmail.ca>
2023-06-22 00:00:56 -0400

    test

--- master/arduino/test.py
+++ master/arduino/test.py
@@ -1,19 +1,11 @@
-def   add_numbers ( x, y):
-   return x   +y
+def     greet(name):
+    print("Hello, " +name+"!")
 
 
-def   main   ( )   :
-number1 = 10
-number2   = 5
-
-   result=  add_numbers(number1 ,number2   )
-print("The   result is:", result)
+def     main():
+    name =    input("Enter your name: ")
+    greet(name)
 
 
-if result > 15   :
-print("Greater  than 15")
-else:
-   print("Less  than  or  equal  to 15")
-
-
-main()
+if __name__ == "__main__":
+    main()
