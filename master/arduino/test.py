eaf54d1f552a2a8994943661a02883408cce7587
Olivier <olivier.jackson@hotmail.ca>
2023-06-21 23:50:12 -0400

    test

---
+++ master/arduino/test.py
@@ -0,0 +1,19 @@
+def   add_numbers ( x, y):
+   return x   +y
+
+
+def   main   ( )   :
+number1 = 10
+number2   = 5
+
+   result=  add_numbers(number1 ,number2   )
+print("The   result is:", result)
+
+
+if result > 15   :
+print("Greater  than 15")
+else:
+   print("Less  than  or  equal  to 15")
+
+
+main()
