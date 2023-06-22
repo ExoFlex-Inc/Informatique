a81e65b1ff3cd7a81a18c6feec7601038fc21792
BigJack325 <BigJack325@users.noreply.github.com>
2023-06-22 04:11:57 +0000

    Apply Formatting changes

--- master/arduino/downloadedLibraries/megaatmega2560/SoftTimer/SoftTimer.h
+++ master/arduino/downloadedLibraries/megaatmega2560/SoftTimer/SoftTimer.h
@@ -10,52 +10,51 @@ Class to start a function after a certain delay or at certain frequency
 
 #include <Arduino.h>
 
-class SoftTimer
-{
-  public:
-    SoftTimer();
+class SoftTimer {
+public:
+  SoftTimer();
 
-    /** Function to set a delay between callback of a timer
+  /** Function to set a delay between callback of a timer
 
-    @param delay
-    delay in millisecond between call
-    */
-    void setDelay(unsigned long delay){ delay_ = delay; };
+  @param delay
+  delay in millisecond between call
+  */
+  void setDelay(unsigned long delay) { delay_ = delay; };
 
-    /** Function to a number of repetition of the callback before desabling it
+  /** Function to a number of repetition of the callback before desabling it
 
-    @param nrep
-    number of repetition
-    @note if nrep is negative or is not set, the timer will run forever (default);
-    */
-    void setRepetition(int32_t nrep){ nRep_ = nrep; };
+  @param nrep
+  number of repetition
+  @note if nrep is negative or is not set, the timer will run forever (default);
+  */
+  void setRepetition(int32_t nrep) { nRep_ = nrep; };
 
-    /** Method to set a callback to a timer
+  /** Method to set a callback to a timer
 
-    @param function to set
-    A function that returns void with no parameters
-    */
-    void setCallback(void (*func)()){f_ = func;};
+  @param function to set
+  A function that returns void with no parameters
+  */
+  void setCallback(void (*func)()) { f_ = func; };
 
-    /** Method to call callback if callTime_ is passed
+  /** Method to call callback if callTime_ is passed
 
-    @note the next callTime_ and nRep_ is computed if necessary
-    */
-    void update();
-    /** Function to a enable timer
-    */
-    void enable();
+  @note the next callTime_ and nRep_ is computed if necessary
+  */
+  void update();
+  /** Function to a enable timer
+   */
+  void enable();
 
-    /** Function to a disable timer
-    */
-    void disable();
+  /** Function to a disable timer
+   */
+  void disable();
 
-  private:
-    void (*f_)(); // void, parameterless, function pointer
-    unsigned long delay_; // Delay (ms) between callback
-    unsigned long callTime_; // Time at which to call function
-    bool enable_; // Timer state
-    int32_t nRep_; // Number of iteration (-1 is infinite)
-    int32_t currRep_;// Number of iteration left
+private:
+  void (*f_)();            // void, parameterless, function pointer
+  unsigned long delay_;    // Delay (ms) between callback
+  unsigned long callTime_; // Time at which to call function
+  bool enable_;            // Timer state
+  int32_t nRep_;           // Number of iteration (-1 is infinite)
+  int32_t currRep_;        // Number of iteration left
 };
-#endif //SoftTimer
+#endif // SoftTimer
--- master/arduino/src/main.cpp
+++ master/arduino/src/main.cpp
@@ -15,13 +15,14 @@
  ****************************************************/
 
 /*------------------------------ Librairies ---------------------------------*/
-#include <SoftwareSerial.h>
-#include <ArduinoJson.h>
 #include <AccelStepper.h>
-#include <SoftTimer.h>
+#include <ArduinoJson.h>
 #include <Servo.h>
+#include <SoftTimer.h>
+#include <SoftwareSerial.h>
 #include <string.h>
-/*------------------------------ Global Constantes ---------------------------------*/
+/*------------------------------ Global Constantes
+ * ---------------------------------*/
 using namespace std;
 
 #define BAUD 115200        // Frequence de transmission serielle
@@ -47,13 +48,16 @@ using namespace std;
 #define eversion_limit_switch 28
 #define dorsiflex_limit_switch 29
 
-#define tightening_step_resolution 200 // Set desired move: 200 steps (in quater-step resolution that's one rotation)
+#define tightening_step_resolution                                             \
+  200 // Set desired move: 200 steps (in quater-step resolution that's one
+      // rotation)
 
 Servo dorsiflex_motor;               // PIN 8
 Servo eversion_motor;                // PIN 7
 AccelStepper stepper_tight(1, 2, 5); // STEP PIN 2, STEP DIR 5
 
-/*------------------------------ Global Variable ---------------------------------*/
+/*------------------------------ Global Variable
+ * ---------------------------------*/
 volatile bool shouldSend_ = false; // Ready to send message to serial flag
 volatile bool shouldRead_ = false; // Ready to read message to serial flag
 
@@ -80,241 +84,224 @@ void serialEvent() { shouldRead_ = true; }
 
 void timerCallback() { shouldSend_ = true; }
 
-void sendMsg()
-{
+void sendMsg() {
 
-    /* Envoit du message Json sur le port seriel */
-    StaticJsonDocument<500> doc;
+  /* Envoit du message Json sur le port seriel */
+  StaticJsonDocument<500> doc;
 
-    // Elements du message
-    doc["time"] = (millis() / 1000.0);
-    doc["Case"] = command;
-    // doc["DorsiflexAngle"] = dorsiflexMotorCurrentAngle;
-    doc["DorsiflexAngle"] = map(dorsiflexMotorCurrentAngle, 1000, 2000, 0, 180);
-    doc["EversionAngle"] = map(eversionMotorCurrentAngle, 1000, 2000, 0, 180);
-    doc["TighteningAngle"] = map(tighteningCurrentAngle, 0, tightening_step_resolution, 0, 180);
+  // Elements du message
+  doc["time"] = (millis() / 1000.0);
+  doc["Case"] = command;
+  // doc["DorsiflexAngle"] = dorsiflexMotorCurrentAngle;
+  doc["DorsiflexAngle"] = map(dorsiflexMotorCurrentAngle, 1000, 2000, 0, 180);
+  doc["EversionAngle"] = map(eversionMotorCurrentAngle, 1000, 2000, 0, 180);
+  doc["TighteningAngle"] =
+      map(tighteningCurrentAngle, 0, tightening_step_resolution, 0, 180);
 
-    // Serialisation
-    serializeJson(doc, Serial);
+  // Serialisation
+  serializeJson(doc, Serial);
 
-    // Envoit
-    Serial.print("*");
-    shouldSend_ = false;
+  // Envoit
+  Serial.print("*");
+  shouldSend_ = false;
 }
 
-void readMsg()
-{
+void readMsg() {
 
-    // Lecture du message Json
-    StaticJsonDocument<500> doc;
-    JsonVariant parse_msg;
+  // Lecture du message Json
+  StaticJsonDocument<500> doc;
+  JsonVariant parse_msg;
 
-    // Lecture sur le port Seriel
-    DeserializationError error = deserializeJson(doc, Serial);
-    shouldRead_ = false;
+  // Lecture sur le port Seriel
+  DeserializationError error = deserializeJson(doc, Serial);
+  shouldRead_ = false;
 
-    // Si erreur dans le message
-    if (error)
-    {
-        Serial.print("deserialize() failed:");
-        Serial.println(error.c_str());
-        return;
-    }
+  // Si erreur dans le message
+  if (error) {
+    Serial.print("deserialize() failed:");
+    Serial.println(error.c_str());
+    return;
+  }
 
-    parse_msg = doc["Case"];
+  parse_msg = doc["Case"];
 
-    if (!parse_msg.isNull())
-    {
-        command = doc["Case"];
-    }
-    parse_msg = doc["TighteningAngle"];
+  if (!parse_msg.isNull()) {
+    command = doc["Case"];
+  }
+  parse_msg = doc["TighteningAngle"];
 
-    if (!parse_msg.isNull())
-    {
-        tighteningCurrentAngle = doc["TighteningAngle"];
-        tighteningCurrentAngle = map(tighteningCurrentAngle, 0, 180, 0, tightening_step_resolution);
-    }
-    parse_msg = doc["DorsiflexAngle"];
+  if (!parse_msg.isNull()) {
+    tighteningCurrentAngle = doc["TighteningAngle"];
+    tighteningCurrentAngle =
+        map(tighteningCurrentAngle, 0, 180, 0, tightening_step_resolution);
+  }
+  parse_msg = doc["DorsiflexAngle"];
 
-    if (!parse_msg.isNull())
-    {
-        dorsiflexMotorCurrentAngle = map(doc["DorsiflexAngle"], 0, 180, 1000, 2000);
-    }
-    parse_msg = doc["EversionAngle"];
+  if (!parse_msg.isNull()) {
+    dorsiflexMotorCurrentAngle = map(doc["DorsiflexAngle"], 0, 180, 1000, 2000);
+  }
+  parse_msg = doc["EversionAngle"];
 
-    if (!parse_msg.isNull())
-    {
-        eversionMotorCurrentAngle = map(doc["EversionAngle"], 0, 180, 1000, 2000);
-    }
+  if (!parse_msg.isNull()) {
+    eversionMotorCurrentAngle = map(doc["EversionAngle"], 0, 180, 1000, 2000);
+  }
 }
 
-void motorMove(const char *motor, int power)
-{
+void motorMove(const char *motor, int power) {
 
-    // Remap power percent to motor speed values(0 = full reverse, 90 = stop, 180 = full forward)
-    if (power >= 100)
-    {
-        power = 180;
+  // Remap power percent to motor speed values(0 = full reverse, 90 = stop, 180
+  // = full forward)
+  if (power >= 100) {
+    power = 180;
+  } else if (power <= -100) {
+    power = 0;
+  } else {
+    power = map(power, -100, 100, 0, 180);
+  }
+
+  if (motor == "dorsiflex") {
+    if (power == 90) {
+
+      dorsiflex_motor.write(power);
+      return;
     }
-    else if (power <= -100)
-    {
-        power = 0;
+
+    // Move motor
+    dorsiflex_motor.write(power);
+
+    // Update Encoder
+    dorsiflexState = digitalRead(
+        dorsiflex_channel_A); // Reads the "current" state of the channel_A
+
+    // If the previous and the current state of the channel_A are different,
+    // that means a Pulse has occured
+    if (dorsiflexState != dorsiflexLastState) {
+      // If the outputB state is different to the outputA state, that means the
+      // encoder is rotating clockwise
+      if (digitalRead(dorsiflex_channel_B) != dorsiflexState) {
+        dorsiflexMotorCurrentAngle++;
+      } else {
+        dorsiflexMotorCurrentAngle--;
+      }
     }
-    else
-    {
-        power = map(power, -100, 100, 0, 180);
+
+    dorsiflexLastState = dorsiflexState; // Updates the previous state of the
+                                         // channel_A with the current state
+  } else if (motor == "eversion") {
+    if (power == 90) {
+      eversion_motor.write(power);
+      return;
     }
 
-    if (motor == "dorsiflex")
-    {
-        if (power == 90)
-        {
+    // Move motor
+    eversion_motor.write(power);
 
-            dorsiflex_motor.write(power);
-            return;
-        }
+    // Update Encoder
+    eversionState = digitalRead(
+        eversion_channel_A); // Reads the "current" state of the channel_A
 
-        // Move motor
-        dorsiflex_motor.write(power);
-
-        // Update Encoder
-        dorsiflexState = digitalRead(dorsiflex_channel_A); // Reads the "current" state of the channel_A
-
-        // If the previous and the current state of the channel_A are different, that means a Pulse has occured
-        if (dorsiflexState != dorsiflexLastState)
-        {
-            // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-            if (digitalRead(dorsiflex_channel_B) != dorsiflexState)
-            {
-                dorsiflexMotorCurrentAngle++;
-            }
-            else
-            {
-                dorsiflexMotorCurrentAngle--;
-            }
-        }
-
-        dorsiflexLastState = dorsiflexState; // Updates the previous state of the channel_A with the current state
+    // If the previous and the current state of the channel_A are different,
+    // that means a Pulse has occured
+    if (eversionState != eversionLastState) {
+      // If the outputB state is different to the outputA state, that means the
+      // encoder is rotating clockwise
+      if (digitalRead(eversion_channel_B) != eversionState) {
+        eversionMotorCurrentAngle++;
+      } else {
+        eversionMotorCurrentAngle--;
+      }
     }
-    else if (motor == "eversion")
-    {
-        if (power == 90)
-        {
-            eversion_motor.write(power);
-            return;
-        }
 
-        // Move motor
-        eversion_motor.write(power);
-
-        // Update Encoder
-        eversionState = digitalRead(eversion_channel_A); // Reads the "current" state of the channel_A
-
-        // If the previous and the current state of the channel_A are different, that means a Pulse has occured
-        if (eversionState != eversionLastState)
-        {
-            // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-            if (digitalRead(eversion_channel_B) != eversionState)
-            {
-                eversionMotorCurrentAngle++;
-            }
-            else
-            {
-                eversionMotorCurrentAngle--;
-            }
-        }
-
-        eversionLastState = eversionState; // Updates the previous state of the channel_A with the current state
-    }
-    // Serial.println(power);
-    // Serial.println("--------------");
-    // Serial.println(digitalRead(dorsiflex_channel_A));
-    // Serial.println(digitalRead(dorsiflex_channel_B));
-    // Serial.println("--------------");
-    // Serial.println(dorsiflexMotorCurrentAngle);
-    // Serial.println("--------------");
+    eversionLastState = eversionState; // Updates the previous state of the
+                                       // channel_A with the current state
+  }
+  // Serial.println(power);
+  // Serial.println("--------------");
+  // Serial.println(digitalRead(dorsiflex_channel_A));
+  // Serial.println(digitalRead(dorsiflex_channel_B));
+  // Serial.println("--------------");
+  // Serial.println(dorsiflexMotorCurrentAngle);
+  // Serial.println("--------------");
 }
 
 /*---------------------------Setup------------------------*/
 
-void setup()
-{
-    Serial.begin(BAUD); // Initialisation of serial communication
+void setup() {
+  Serial.begin(BAUD); // Initialisation of serial communication
 
-    // Send message timer
-    timerSendMsg_.setDelay(UPDATE_PERIODE);
-    timerSendMsg_.setCallback(timerCallback);
-    timerSendMsg_.enable();
+  // Send message timer
+  timerSendMsg_.setDelay(UPDATE_PERIODE);
+  timerSendMsg_.setCallback(timerCallback);
+  timerSendMsg_.enable();
 
-    // Servo init
-    // With all three arguments, we can use 0 to 180 degrees, with 90 being stopped.
-    dorsiflex_motor.attach(dorsiflex_enable, 1000, 2000); // dorsiflexion motor attached to PIN 8
-    eversion_motor.attach(eversion_enable, 1000, 2000);   // eversion motor attached to PIN 7
+  // Servo init
+  // With all three arguments, we can use 0 to 180 degrees, with 90 being
+  // stopped.
+  dorsiflex_motor.attach(dorsiflex_enable, 1000,
+                         2000); // dorsiflexion motor attached to PIN 8
+  eversion_motor.attach(eversion_enable, 1000,
+                        2000); // eversion motor attached to PIN 7
 
-    dorsiflex_motor.write(90);
-    eversion_motor.write(90);
+  dorsiflex_motor.write(90);
+  eversion_motor.write(90);
 
-    pinMode(dorsiflex_channel_A, INPUT);
-    pinMode(dorsiflex_channel_B, INPUT);
-    pinMode(eversion_channel_A, INPUT);
-    pinMode(eversion_channel_B, INPUT);
+  pinMode(dorsiflex_channel_A, INPUT);
+  pinMode(dorsiflex_channel_B, INPUT);
+  pinMode(eversion_channel_A, INPUT);
+  pinMode(eversion_channel_B, INPUT);
 
-    pinMode(dorsiflex_limit_switch, INPUT);
-    pinMode(eversion_limit_switch, INPUT);
+  pinMode(dorsiflex_limit_switch, INPUT);
+  pinMode(eversion_limit_switch, INPUT);
 
-    dorsiflexLastState = digitalRead(dorsiflex_channel_A);
-    eversionLastState = digitalRead(eversion_channel_A);
+  dorsiflexLastState = digitalRead(dorsiflex_channel_A);
+  eversionLastState = digitalRead(eversion_channel_A);
 
-    // Stepper init
-    stepper_tight.setMaxSpeed(1000);     // Set maximum speed value for the stepper
-    stepper_tight.setAcceleration(500);  // Set acceleration value for the stepper
-    stepper_tight.setCurrentPosition(0); // Set the current position to 0 steps
-    stepper_tight.disableOutputs();
+  // Stepper init
+  stepper_tight.setMaxSpeed(1000);    // Set maximum speed value for the stepper
+  stepper_tight.setAcceleration(500); // Set acceleration value for the stepper
+  stepper_tight.setCurrentPosition(0); // Set the current position to 0 steps
+  stepper_tight.disableOutputs();
 }
 
 /*------------------------------ Main loop ---------------------------------*/
-void loop()
-{
-    if (shouldRead_)
-    {
-        readMsg();
-    }
-    if (shouldSend_)
-    {
-        sendMsg();
-    }
+void loop() {
+  if (shouldRead_) {
+    readMsg();
+  }
+  if (shouldSend_) {
+    sendMsg();
+  }
 
-    //---------------------- SWITCH CASE -------------------------------
-    switch (command)
-    {
-    case INITIALIZE:
-        break;
-    case WAIT:
-        motorMove("dorsiflex", 0);
-        motorMove("eversion", 0);
-        break;
+  //---------------------- SWITCH CASE -------------------------------
+  switch (command) {
+  case INITIALIZE:
+    break;
+  case WAIT:
+    motorMove("dorsiflex", 0);
+    motorMove("eversion", 0);
+    break;
 
-    case DORSIFLEXION_UP: // Sets all servomotors to initial angles
-        motorMove("dorsiflex", 100);
-        break;
+  case DORSIFLEXION_UP: // Sets all servomotors to initial angles
+    motorMove("dorsiflex", 100);
+    break;
 
-    case DORSIFLEXION_DOWN: // Sets all servomotors to initial angles
-        motorMove("dorsiflex", -100);
-        break;
+  case DORSIFLEXION_DOWN: // Sets all servomotors to initial angles
+    motorMove("dorsiflex", -100);
+    break;
 
-    case EVERSION_LEFT: // Sets all servomotors to initial angles
-        motorMove("eversion", 50);
-        eversionMotorCurrentAngle += 1;
-        break;
+  case EVERSION_LEFT: // Sets all servomotors to initial angles
+    motorMove("eversion", 50);
+    eversionMotorCurrentAngle += 1;
+    break;
 
-    case EVERSION_RIGHT: // Sets all servomotors to initial angles
-        motorMove("eversion", -50);
-        break;
-    case TIGHTENING:
-        stepper_tight.moveTo(tighteningCurrentAngle);
-        stepper_tight.run();
-        break;
-    }
+  case EVERSION_RIGHT: // Sets all servomotors to initial angles
+    motorMove("eversion", -50);
+    break;
+  case TIGHTENING:
+    stepper_tight.moveTo(tighteningCurrentAngle);
+    stepper_tight.run();
+    break;
+  }
 
-    timerSendMsg_.update();
+  timerSendMsg_.update();
 }
