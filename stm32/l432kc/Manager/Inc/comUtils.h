b2fa060d462a1813d2c105c629f3e2cd645d5354
BigJack325 <BigJack325@users.noreply.github.com>
2023-09-19 15:12:27 +0000

    Apply Formatting changes

--- stm32/l432kc/Manager/Inc/comUtils.h
+++ stm32/l432kc/Manager/Inc/comUtils.h
@@ -4,23 +4,20 @@
 
 #include <stdint.h>
 
-uint8_t checkString (char *str, char *buffertolookinto);
+uint8_t checkString(char* str, char* buffertolookinto);
 
 /* waits for a particular string in the Rx Buffer
- * By Default it is set to wait for "OK", you can change the string in the HAL_UARTEx_RxEventCallback function
- * This function will wait in the blocking mode, so to avoid the halt, we will also include the timeout
- * The timeout is in milliseconds
- * returns 1 on success
- * returns 0 on failure
+ * By Default it is set to wait for "OK", you can change the string in the
+ * HAL_UARTEx_RxEventCallback function This function will wait in the blocking
+ * mode, so to avoid the halt, we will also include the timeout The timeout is
+ * in milliseconds returns 1 on success returns 0 on failure
  * */
-uint8_t isConfirmed (int32_t Timeout);
-
+uint8_t isConfirmed(int32_t Timeout);
 
 /* Waits for a particular string to arrive in the incoming buffer
  * returns 1, if the string is detected
  */
-int waitFor (char *string, uint32_t Timeout);
-
+int waitFor(char* string, uint32_t Timeout);
 
 /* copies the data from the incoming buffer into the provided buffer
  * Must be used if you are sure that the data is being received
@@ -28,17 +25,17 @@ int waitFor (char *string, uint32_t Timeout);
  * if the end string gets copied, it returns 1 or else 0
  *
  */
-int copyUpto (char *string, char *buffertocopyinto, uint32_t Timeout);
+int copyUpto(char* string, char* buffertocopyinto, uint32_t Timeout);
 
-
-/* Copies the entered number of characters, after the entered string (from the incoming buffer), into the buffer
- * returns 1 on success
- * returns 0 on failure
+/* Copies the entered number of characters, after the entered string (from the
+ * incoming buffer), into the buffer returns 1 on success returns 0 on failure
  */
-int getAfter (char *string, uint8_t numberofchars, char *buffertocopyinto, uint32_t Timeout);
+int getAfter(char* string, uint8_t numberofchars, char* buffertocopyinto,
+             uint32_t Timeout);
 
-void getDataFromBuffer (char *startString, char *endString, char *buffertocopyfrom, char *buffertocopyinto);
+void getDataFromBuffer(char* startString, char* endString,
+                       char* buffertocopyfrom, char* buffertocopyinto);
 
-char* searchWord(char *buffertocopyfrom);
+char* searchWord(char* buffertocopyfrom);
 
 #endif /* CONMUTILS_H */
--- stm32/l432kc/UnitTests/test/test_comUtils.c
+++ stm32/l432kc/UnitTests/test/test_comUtils.c
@@ -1,8 +1,9 @@
 // Include necessary headers for your implementation and Unity
 // #include "mock_uartRingBufDMA.h"
+#include <string.h>
+
 #include "comUtils.h"
 #include "unity.h"
-#include <string.h>
 
 uint16_t oldPos = 0;
 uint16_t newPos = 0;
@@ -18,7 +19,8 @@ int32_t TIMEOUT = 0;
 
 char MainBuf[100];
 
-void test_searchWord_unfinishedWord(void) {
+void test_searchWord_unfinishedWord(void)
+{
     // Mocked MainBuf for testing
     strcpy(MainBuf, "ightrightleft");
 
@@ -30,7 +32,8 @@ void test_searchWord_unfinishedWord(void) {
     TEST_ASSERT_EQUAL_STRING("left", MainBuf);
 }
 
-void test_searchWord_emptyBuffer(void) {
+void test_searchWord_emptyBuffer(void)
+{
     // Mocked MainBuf for testing
     strcpy(MainBuf, "");
 
@@ -42,7 +45,8 @@ void test_searchWord_emptyBuffer(void) {
     TEST_ASSERT_EQUAL_STRING("", MainBuf);
 }
 
-void test_searchWord_noMatch(void) {
+void test_searchWord_noMatch(void)
+{
     // Mocked MainBuf for testing
     strcpy(MainBuf, "abcdefgh");
 
@@ -54,7 +58,8 @@ void test_searchWord_noMatch(void) {
     TEST_ASSERT_EQUAL_STRING("", MainBuf);
 }
 
-void test_searchWord_multipleOccurrences(void) {
+void test_searchWord_multipleOccurrences(void)
+{
     // Mocked MainBuf for testing
     strcpy(MainBuf, "leftleftright");
 
