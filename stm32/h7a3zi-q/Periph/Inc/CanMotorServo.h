68eb314ec9f78b163c669b68bc7163502fd5ffd2
BigJack325 <BigJack325@users.noreply.github.com>
2024-02-15 16:17:36 +0000

    Apply Formatting changes

--- stm32/h7a3zi-q/Periph/Inc/CanMotorServo.h
+++ stm32/h7a3zi-q/Periph/Inc/CanMotorServo.h
@@ -2,7 +2,6 @@
 
 #include "main.h"
 
-
 typedef enum
 {
     CAN_PACKET_SET_DUTY = 0,  // Duty cycle mode
@@ -14,11 +13,12 @@ typedef enum
     CAN_PACKET_POS_SPD  // Position velocity loop mode
 } CAN_PACKET_ID;
 
-
 void CanMotorServo_Init();
-void CanMotorServo_Receive(float* motor_pos, float* motor_spd, float* motor_cur, uint8_t* motor_temp, uint8_t* motor_error);
+void CanMotorServo_Receive(float* motor_pos, float* motor_spd, float* motor_cur,
+                           uint8_t* motor_temp, uint8_t* motor_error);
 void CanMotorServo_SetOrigin(uint8_t controller_id);
 void CanMotorServo_SetPos(uint8_t controller_id, float pos);
-void CanMotorServo_SetPosSpeed(uint8_t controller_id, float pos, int16_t spd,int16_t RPA);
+void CanMotorServo_SetPosSpeed(uint8_t controller_id, float pos, int16_t spd,
+                               int16_t RPA);
 
 #endif
