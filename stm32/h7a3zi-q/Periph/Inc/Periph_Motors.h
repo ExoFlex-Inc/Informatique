6ce946b2ac17c0c956f3a04ffd619365056a1d5c
NicAU9 <NicAU9@users.noreply.github.com>
2024-03-03 21:38:24 +0000

    Apply Formatting changes

--- stm32/h7a3zi-q/Periph/Inc/Periph_Motors.h
+++ stm32/h7a3zi-q/Periph/Inc/Periph_Motors.h
@@ -13,43 +13,43 @@
 #define MOTOR_AK10_9  0
 #define MOTOR_AK80_64 1
 
-    typedef void (*SendCanDataFunction)(uint32_t id, uint8_t* data);
+typedef void (*SendCanDataFunction)(uint32_t id, uint8_t* data);
 
-    typedef struct
-    {
-        float positionMin;
-        float positionMax;
+typedef struct
+{
+    float positionMin;
+    float positionMax;
 
-        float velocityMin;
-        float velocityMax;
+    float velocityMin;
+    float velocityMax;
 
-        float torqueMin;
-        float torqueMax;
+    float torqueMin;
+    float torqueMax;
 
-        float kpMin;
-        float kpMax;
+    float kpMin;
+    float kpMax;
 
-        float kdMin;
-        float kdMax;
-    } MotorParameters;
+    float kdMin;
+    float kdMax;
+} MotorParameters;
 
-    typedef struct
-    {
-        uint8_t         id;
-        float           position;
-        float           velocity;
-        float           torque;
-        MotorParameters parameters;
-    } Motor;
+typedef struct
+{
+    uint8_t         id;
+    float           position;
+    float           velocity;
+    float           torque;
+    MotorParameters parameters;
+} Motor;
 
-    void PeriphMotors_Init(SendCanDataFunction sendCanFunc);
-    bool PeriphMotors_InitMotor(Motor* pMotor, uint8_t id, uint8_t model);
-    void PeriphMotors_Enable(Motor* pMotor);
-    void PeriphMotors_SubscribeToRx(Motor* pMotor);
-    void PeriphMotors_Disable(Motor* pMotor);
-    void PeriphMotors_SetZeroPosition(Motor* pMotor);
-    void PeriphMotors_Move(Motor* pMotor, float position, float velocity,
-                           float torque, float kp, float kd);
-    void PeriphMotors_ParseMotorState(Motor* pMotor, uint8_t* canData);
+void PeriphMotors_Init(SendCanDataFunction sendCanFunc);
+bool PeriphMotors_InitMotor(Motor* pMotor, uint8_t id, uint8_t model);
+void PeriphMotors_Enable(Motor* pMotor);
+void PeriphMotors_SubscribeToRx(Motor* pMotor);
+void PeriphMotors_Disable(Motor* pMotor);
+void PeriphMotors_SetZeroPosition(Motor* pMotor);
+void PeriphMotors_Move(Motor* pMotor, float position, float velocity,
+                       float torque, float kp, float kd);
+void PeriphMotors_ParseMotorState(Motor* pMotor, uint8_t* canData);
 
 #endif /* INC_PERIPH_MOTORS_H_ */
--- stm32/h7a3zi-q/Periph/Inc/comUtils_UART2.h
+++ stm32/h7a3zi-q/Periph/Inc/comUtils_UART2.h
@@ -4,11 +4,11 @@
 
 #include <stdint.h>
 
-    /* Search for a word in a wordlist inside the buffer and deletes unfound
-     * words returns the closest word found on success returns NULL if no word
-     * found
-     */
+/* Search for a word in a wordlist inside the buffer and deletes unfound
+ * words returns the closest word found on success returns NULL if no word
+ * found
+ */
 
-    char* searchWord(char* buffertocopyfrom);
+char* searchWord(char* buffertocopyfrom);
 
 #endif /* CONMUTILS_UART2_H */
