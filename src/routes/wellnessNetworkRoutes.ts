d92fcaec2e0278427f15b33831ec1beb75128da0
Patdup <Patdup@users.noreply.github.com>
2024-06-25 21:13:22 +0000

    Apply Formatting changes

--- src/routes/wellnessNetworkRoutes.ts
+++ src/routes/wellnessNetworkRoutes.ts
@@ -1,10 +1,21 @@
 import express from "express";
-import { postAdminClientsList, getAdminClientsList } from "../controllers/wellnessNetworkController.ts";
+import {
+  postAdminClientsList,
+  getAdminClientsList,
+} from "../controllers/wellnessNetworkController.ts";
 import { checkPermission } from "../middlewares/checkPermission.tsx";
 
 const router = express.Router();
 
-router.post("/wellness_network", checkPermission(['dev', 'admin']), postAdminClientsList);
-router.get("/wellness_network", checkPermission(['dev', 'admin']), getAdminClientsList);
+router.post(
+  "/wellness_network",
+  checkPermission(["dev", "admin"]),
+  postAdminClientsList,
+);
+router.get(
+  "/wellness_network",
+  checkPermission(["dev", "admin"]),
+  getAdminClientsList,
+);
 
 export default router;
