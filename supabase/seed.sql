99c826d083700b474b08fee685ea7ed07ac88624
Pat groulx <patricedupere@gmail.com>
2024-07-15 15:32:09 -0400

    Adding dev to seeder

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -193,4 +193,65 @@ BEGIN
 
     END LOOP;
 
+    new_email := 'dev@example.com';
+    new_id := gen_random_uuid();
+    base_date := NOW() - INTERVAL '1 month'; 
+
+    INSERT INTO auth.users (
+        instance_id, 
+        id, 
+        aud, 
+        role, 
+        email, 
+        encrypted_password, 
+        email_confirmed_at, 
+        recovery_sent_at, 
+        last_sign_in_at, 
+        raw_app_meta_data, 
+        raw_user_meta_data, 
+        created_at, 
+        updated_at,
+        confirmation_token, 
+        email_change, 
+        email_change_token_new, 
+        recovery_token
+    )
+    VALUES (
+        '00000000-0000-0000-0000-000000000000', 
+        new_id, 
+        'authenticated', 
+        'authenticated', 
+        new_email, 
+        crypt('password123', gen_salt('bf')), 
+        '2023-05-03 19:41:43.585805+00', 
+        '2023-04-22 13:10:03.275387+00', 
+        '2023-04-22 13:10:31.458239+00', 
+        '{"provider":"email","providers":["email"]}', 
+        '{}', 
+        NOW(), 
+        NOW(),
+        '', 
+        '', 
+        '', 
+        ''
+    );
+
+    -- Insert into user_profiles
+    INSERT INTO user_profiles (
+        user_id, 
+        username, 
+        lastname, 
+        speciality, 
+        permissions,
+        email
+    ) 
+    VALUES (
+        new_id, -- Ensuring the same UUID is used
+        'Dev',
+        'Lastname',
+        'Developper',
+        'dev',
+        new_email
+    );
+
 END $$;
