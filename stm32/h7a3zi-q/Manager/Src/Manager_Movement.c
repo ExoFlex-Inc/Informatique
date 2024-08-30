91c54a4d9b187f5ede33fbd2a1bf953bb43a6652
NicAU9 <98042317+NicAU9@users.noreply.github.com>
2024-08-30 10:38:43 -0400

    uint bug fix. Tested and it works

--- stm32/h7a3zi-q/Manager/Src/Manager_Movement.c
+++ stm32/h7a3zi-q/Manager/Src/Manager_Movement.c
@@ -86,7 +86,7 @@ void ManagerMovement_HomingDorsiflexion();
 void ManagerMovement_RestPos();
 
 // General movement functions
-bool ManagerMovement_GoToPos(uint8_t exerciseId, uint8_t pos);
+bool ManagerMovement_GoToPos(uint8_t exerciseId, int8_t pos);
 void ManagerMovement_AutoMovement(uint8_t mouvType, float Position);
 void ManagerMovement_SetFirstPos(uint8_t exerciseIdx);
 float ManagerMovement_GetMiddlePos(float leftPos, float rightPos);
@@ -206,7 +206,7 @@ void ManagerMovement_Homing()
         break;
 
     case MMOV_HOMING_REST_POS:
-//        ManagerMovement_RestPos();
+    	ManagerMovement_RestPos();
         break;
     }
 }
@@ -683,7 +683,7 @@ void ManagerMovement_RestPos()
     if (ManagerMovement_GoToPos(MMOV_EXTENSION, MMOV_REST_POS))
     {
         managerMovement.homingState = MMOV_VERIF_PERSON_IN;
-        //managerMovement.state       = MMOV_STATE_MANUAL;
+        managerMovement.state       = MMOV_STATE_MANUAL;
     }
 }
 
@@ -701,7 +701,7 @@ void ManagerMovement_SetOrigins(uint8_t motorIndex)
     managerMovement.motorsNextGoal[motorIndex] = 0.0f;
 }
 
-bool ManagerMovement_GoToPos(uint8_t exerciseId, uint8_t pos)
+bool ManagerMovement_GoToPos(uint8_t exerciseId, int8_t pos)
 {
 	bool posReached = false;
 
@@ -758,7 +758,7 @@ bool ManagerMovement_SetState(uint8_t newState)
 			managerMovement.homingState = MMOV_VERIF_PERSON_IN;
 			stateChanged = true;
 		}
-		else if (newState == MMOV_STATE_MANUAL /*&& managerMovement.state != MMOV_STATE_HOMING*/)
+		else if (newState == MMOV_STATE_MANUAL && managerMovement.state != MMOV_STATE_HOMING)
 		{
 			stateChanged = true;
 		}
