e7174f08d25a72ad6c5c060cc6322766dac74aa2
Pat groulx <patricedupere@gmail.com>
2024-09-05 17:38:05 -0400

    solving the plan init in HMI page

--- src/hooks/get-plan.ts
+++ src/hooks/get-plan.ts
@@ -1,29 +1,36 @@
 import { useState, useEffect } from "react";
 
-const usePlanData = () => {
+const usePlanData = (profile: any) => {
   const [planData, setPlanData] = useState(null);
   const [get_plan, setRetryGet] = useState(true);
 
   useEffect(() => {
     const getPlanRequests = async () => {
       try {
-        const responseGetPlanning = await fetch(
-          "http://localhost:3001/api/plan",
-          {
-            method: "GET",
-          },
-        );
-
-        if (responseGetPlanning.ok) {
-          console.log("Plan retrieved successfully.");
-          const responsePlanData = await responseGetPlanning.json();
-          console.log("Plan data:", responsePlanData[0].plan_content);
-          setPlanData(responsePlanData[0].plan_content);
-          setRetryGet(false);
+        if (profile) {
+          const responseGetPlanning = await fetch(
+            "http://localhost:3001/api/plan",
+            {
+              method: "GET",
+              headers: {
+                UserId: profile.user_id,
+              },
+            },
+          );
+  
+          if (responseGetPlanning.ok) {
+            console.log("Plan retrieved successfully.");
+            const responsePlanData = await responseGetPlanning.json();
+            console.log("Plan data:", responsePlanData[0].plan_content);
+            setPlanData(responsePlanData[0].plan_content);
+            setRetryGet(false);
+          } else {
+            console.error("Failed to retrieve plan.");
+            window.alert("Failed to retrieve plan.");
+            setRetryGet(true);
+          }
         } else {
-          console.error("Failed to retrieve plan.");
-          window.alert("Failed to retrieve plan.");
-          setRetryGet(true);
+          console.error("No user id provided for plan request");
         }
       } catch (error) {
         console.error("An error occurred:", error);
