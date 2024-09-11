8e17cf5be3422aa2a2a15d4808a066516ec0cba1
BigJack325 <olivier.jackson@hotmail.ca>
2024-09-11 17:12:47 -0400

    chore: change supabase .env names

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,10 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.VITE_SUPABASE_API_URL;
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_API_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 const supaClient = createClient(supabaseUrl, supabaseKey);
 
-// Export the client to be used throughout the app
-export const supaAuthClient = supaClient.auth.admin;
 export default supaClient;
