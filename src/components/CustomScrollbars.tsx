417194f3476c9e65b739e0cb93d0519664fb8058
Pat groulx <patricedupere@gmail.com>
2024-09-11 18:19:11 -0400

    Adding custom scrollbar to pages

---
+++ src/components/CustomScrollbars.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import { Scrollbar } from 'react-scrollbars-custom';
+
+interface CustomScrollbarProps {
+  children: ReactNode;
+}
+
+const CustomScrollbar: React.FC<CustomScrollbarProps> = ({ children }) => {
+  return (
+    <Scrollbar
+      thumbYProps={{ style: { backgroundColor: '#2BB673'} }}
+      trackYProps={{ style: { backgroundColor: '#2B5BB6', margin: "5px"} }} 
+    >
+      {children}
+    </Scrollbar>
+  );
+};
+
+export default CustomScrollbar;
--- src/pages/Manual.tsx
+++ src/pages/Manual.tsx
@@ -5,6 +5,7 @@ import ComputerRoundedIcon from "@mui/icons-material/ComputerRounded";
 import MotorControlWidget from "../components/MotorControlWidget.tsx";
 import useStm32 from "../hooks/use-stm32.ts";
 import { useMediaQuery } from "@mui/material";
+import CustomScrollbar from "../components/CustomScrollbars.tsx";
 
 export default function Manual() {
   const { socket, errorFromStm32 } = useStm32();
@@ -31,56 +32,58 @@ export default function Manual() {
 
   return (
     <div className="flex flex-col custom-height overflow-auto">
-      <div className="justify-center flex mb-10">
-        <LineChart
-          chartData={manualData}
-          mode="Manual"
-          type="realtime"
-          socket={socket}
-        />
-      </div>
-      <div className="flex justify-center h-80 mb-4">
-        <MotorControlWidget
-          title={"Motor Control"}
-          icon={<ComputerRoundedIcon sx={{ fontSize: "56px" }} />}
-          labels={[
-            "Motor1H",
-            "Motor1AH",
-            "Motor2H",
-            "Motor2AH",
-            "Motor3H",
-            "Motor3AH",
-          ]}
-          mode="Manual"
-          action="Increment"
-          disabled={errorFromStm32}
-        />
+      <CustomScrollbar>
+        <div className="justify-center flex mb-10">
+          <LineChart
+            chartData={manualData}
+            mode="Manual"
+            type="realtime"
+            socket={socket}
+          />
+        </div>
+        <div className="flex justify-center h-80 mb-4">
+          <MotorControlWidget
+            title={"Motor Control"}
+            icon={<ComputerRoundedIcon sx={{ fontSize: "56px" }} />}
+            labels={[
+              "Motor1H",
+              "Motor1AH",
+              "Motor2H",
+              "Motor2AH",
+              "Motor3H",
+              "Motor3AH",
+            ]}
+            mode="Manual"
+            action="Increment"
+            disabled={errorFromStm32}
+          />
 
-        <MotorControlWidget
-          title={"Anatomical Movement"}
-          icon={<AirlineSeatLegroomExtraIcon sx={{ fontSize: "56px" }} />}
-          labels={[
-            "EversionL",
-            "EversionR",
-            "DorsiflexionU",
-            "DorsiflexionD",
-            "ExtensionU",
-            "ExtensionD",
-          ]}
-          mode="Manual"
-          action="Increment"
-          disabled={errorFromStm32}
-        />
+          <MotorControlWidget
+            title={"Anatomical Movement"}
+            icon={<AirlineSeatLegroomExtraIcon sx={{ fontSize: "56px" }} />}
+            labels={[
+              "EversionL",
+              "EversionR",
+              "DorsiflexionU",
+              "DorsiflexionD",
+              "ExtensionU",
+              "ExtensionD",
+            ]}
+            mode="Manual"
+            action="Increment"
+            disabled={errorFromStm32}
+          />
 
-        <MotorControlWidget
-          title={"Home Settings"}
-          icon={<HomeOutlinedIcon sx={{ fontSize: "56px" }} />}
-          labels={["GoHome1", "GoHome2", "GoHome3", "GoHome", "SetHome"]}
-          mode="Manual"
-          action="Homing"
-          disabled={errorFromStm32}
-        />
-      </div>
+          <MotorControlWidget
+            title={"Home Settings"}
+            icon={<HomeOutlinedIcon sx={{ fontSize: "56px" }} />}
+            labels={["GoHome1", "GoHome2", "GoHome3", "GoHome", "SetHome"]}
+            mode="Manual"
+            action="Homing"
+            disabled={errorFromStm32}
+          />
+        </div>
+      </CustomScrollbar>
     </div>
   );
 }
