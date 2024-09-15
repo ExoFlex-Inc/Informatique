e206470285df5d17f935d4703a5e6cc31cc5400e
BigJack325 <olivier.jackson@hotmail.ca>
2024-09-15 16:41:21 -0400

    fix: pass the keys directly

--- src/utils/firebaseClient.ts
+++ src/utils/firebaseClient.ts
@@ -3,14 +3,15 @@ import { getMessaging } from 'firebase/messaging';
 
 // Firebase configuration object
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAQQjCJyLSZBXgBM3D6G5BxqfurfpS_OP4",
+  authDomain: "exoflex-46e22.firebaseapp.com",
+  projectId: "exoflex-46e22",
+  storageBucket: "exoflex-46e22.appspot.com",
+  messagingSenderId: "361171602737",
+  appId: "1:361171602737:web:8c93faef025bad18bd7410",
+  measurementId: "G-R0PPB7W550"
 };
+
 // Initialize Firebase app client
 const fireClient = initializeApp(firebaseConfig);
 
