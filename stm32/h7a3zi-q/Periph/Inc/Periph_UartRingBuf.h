3b755cbb95a5628157939cb5bcdb98a22c5ff8f1
NicAU9 <98042317+NicAU9@users.noreply.github.com>
2024-09-17 18:30:59 -0400

    Ligne de trop

--- stm32/h7a3zi-q/Periph/Inc/Periph_UartRingBuf.h
+++ stm32/h7a3zi-q/Periph/Inc/Periph_UartRingBuf.h
@@ -21,7 +21,6 @@ void PeriphUartRingBuf_Init();
 void PeriphUartRingBuf_Read(char* buf, uint32_t* size);
 void PeriphUartRingBuf_Send(char* buf, uint32_t size);
 void PeriphUartRingBuf_ReadJson(char* buf, uint32_t* size);
-void PeriphUartRingBuf_ReadJson(char* buf, uint32_t* size);
 uint32_t PeriphUartRingBuf_GetRxTimerDelay();
 
 #endif /* INC_PERIPH_UARTRINGBUF_H_ */
